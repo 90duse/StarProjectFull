1f3ca12b695e72887bd6e8fc775d1082b93972b3
90duse <duseibnuduse1212@gmail.com>
2023-05-15 17:02:14 +0300

    added some changes

--- template/config/Delete.php
+++ template/config/Delete.php
@@ -3,15 +3,13 @@
 include '../config.php';
 
 
-if(isset($_GET['cr_id'])){
-    $ID = $_GET['cr_id'];
-
-    $sql =   "DELETE FROM crimerecord_table WHERE id= $ID";
-    $result = mysqli_query($con, $sql);
+if(isset($_GET['ID'])){
+    $ID = $_GET['ID']; 
+    $result = mysqli_query($con, "DELETE FROM crimerecord_table WHERE  id=17");
      if($result){
         echo 'workin';
      } else{
-        echo mysqli_error($con);
+        //echo mysqli_error($con);
      }
       
     }
