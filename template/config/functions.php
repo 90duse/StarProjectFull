b24ea2a440a7add337987d6759c612e495ecb7d3
90duse <duseibnuduse1212@gmail.com>
2023-05-20 13:00:07 +0300

    Created Function table to store functions in the future

---
+++ template/config/functions.php
@@ -0,0 +1,14 @@
+<?php
+// PHP program to pop an alert
+// message box on the screen
+function function_alert($message) {
+	
+	// Display the alert box
+	echo "<script>alert('$message');</script>";
+}
+
+
+// Function call
+function_alert("You successfully Deleted");
+
+?>
