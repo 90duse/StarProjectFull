d2499194e2aa2bd31da1f87903f37990a2b92665
90duse <duseibnuduse1212@gmail.com>
2023-05-20 15:10:57 +0300

    Added Some change on the logo

--- template/partials/navbar.php
+++ template/partials/navbar.php
@@ -6,18 +6,20 @@
           </button>
         </div>
         <div>
-          <a class="navbar-brand brand-logo" href="index.php">
-            <img src="../images/logo.svg" alt="logo" />
+          <a class="navbar-brand " href="index.php">
+            <img src="../images/POLICEL.png" width="50" height ="50" alt="logo" />
           </a>
-          <a class="navbar-brand brand-logo-mini" href="index.php">
-            <img src="../images/logo-mini.svg" alt="logo" />
+          <a class="navbar-brand " href="index.php">
+            <img src="../images/POLICEL.png" width="50" height ="50" alt="logo" />
           </a>
+         
+          
         </div>
       </div>
       <div class="navbar-menu-wrapper d-flex align-items-top"> 
         <ul class="navbar-nav">
           <li class="nav-item font-weight-semibold d-none d-lg-block ms-0">
-            <h1 class="welcome-text">Good Morning, <span class="text-black fw-bold">John Doe</span></h1>
+            <h1 class="welcome-text">Good Morning, <span class="text-black fw-bold">OFFICER</span></h1>
             <h3 class="welcome-sub-text">Your performance summary this week </h3>
           </li>
         </ul>
