abc8e4b94025af31e0301f1fba3bce4880178904
90duse <duseibnuduse1212@gmail.com>
2023-05-26 10:21:44 +0300

    created this table for data table testing and it's code is use full but it isnot finished

---
+++ template/config/testingDataTable.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>POLICE MANAGEMENT SYSTEM </title>
+  <!-- plugins:css -->
+  <link rel="stylesheet" href="../vendors/feather/feather.css">
+  <link rel="stylesheet" href="../vendors/mdi/css/materialdesignicons.min.css">
+  <link rel="stylesheet" href="../vendors/ti-icons/css/themify-icons.css">
+  <link rel="stylesheet" href="../vendors/typicons/typicons.css">
+  <link rel="stylesheet" href="../vendors/simple-line-icons/css/simple-line-icons.css">
+  <link rel="stylesheet" href="../vendors/css/vendor.bundle.base.css">
+  <!-- endinject -->
+  <!-- Plugin css for this page -->
+  <link rel="stylesheet" href="../vendors/datatables.net-bs4/dataTables.bootstrap4.css">
+  <link rel="stylesheet" href="../js/select.dataTables.min.css">
+  <!-- End plugin css for this page -->
+  <!-- inject:css -->
+  <link rel="stylesheet" href="../css/vertical-layout-light/style.css">
+  <!-- endinject -->
+  <link rel="shortcut icon" href="../images/favicon.png" />
+ 
+
+    <!-- data table style link -->
+    <link  rel ="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/css/bootstrap.min.css">
+    <link rel = "stylesheet" href ="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css">
+    <link rel="stylesheet" href="../js/select.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
+    
+
+     <!-- js link -->
+<!-- <script defer src ="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script defer src ="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+<script defer src ="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script> -->
+<!-- <script scr="./config/action/dataTableJs.js"></script> -->
+<!-- <script  defer scr="script.js"></script> -->
+<link rel="stylesheet" href="path-to/node_modules/bootstrap-table/dist/bootstrap-table.min.css">
+</head>
+<body>
+ 
+<table data-bs-toggle="table">
+  <thead>
+  <tr>
+    <th>Item ID</th>
+    <th>Item Name</th>
+    <th>Item Price</th>
+    <th>Item ID</th>
+    <th>Item Name</th>
+    <th>Item Price</th>
+    <th>Item ID</th>
+    <th>Item Name</th>
+    <th>Item Price</th>
+  </tr>
+  </thead>
+  <tbody>
+  <tr>
+    <td>1</td>
+    <td>Item 1</td>
+    <td>$1</td>
+    <td>1</td>
+    <td>Item 1</td>
+    <td>$1</td>
+    <td>1</td>
+    <td>Item 1</td>
+    <td>$1</td>
+  </tr>
+  <tr>
+    <td>2</td>
+    <td>Item 2</td>
+    <td>$2</td>
+  </tr>
+  </tbody>
+</table>
+   
+
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+<script src="script.js"></script>
+
+ <script scr="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
+ <script src="path-to/node_modules/bootstrap-table/dist/bootstrap-table.min.js"></script>
+</body>
+
+</html>
