c7bca2ea1a1516ccfa70bd62b792b7a9013c428f
90duse <duseibnuduse1212@gmail.com>
2023-06-06 14:32:42 +0300

    Added data table and some code styling

--- template/config/viewPoliceAsset.php
+++ template/config/viewPoliceAsset.php
@@ -22,6 +22,10 @@
   <link rel="stylesheet" href="../css/vertical-layout-light/style.css">
   <!-- endinject -->
   <link rel="shortcut icon" href="../images/favicon.png" />
+   <!-- data table plugins -->
+   <link rel="stylesheet" href="../css/dataTables.bootstrap5.min.css">
+  <link href="../DataTables/DataTables-1.13.4/css/datatables.bootstrap5.min.css" rel="stylesheet"/>
+  <link href="../DataTables/DataTables-1.13.4/css/bootstrap.min.css" rel="stylesheet"/>
  
 </head>
 <body class="">
@@ -48,217 +52,67 @@
       <div class="main-panel">
         <div class="content-wrapper">
           <div class="row">
-            <div class="col-sm-12">
-              <div class="card">
-                <div class="card-body">
-                  <h4 class="card-title text-center">View Police Asset </h4>
-                  <form action="#" method="GET">
-                  <div class="row flex-grow">
-                          <div class="col-12 grid-margin stretch-card">
-                            <div class="card card-rounded">
-                              <div class="card-body ">
-                                <div class="d-sm-flex justify-content-between align-items-start">
-                                  <div>
-                                    <!-- <h4 class="card-title card-title-dash">Pending Requests</h4> -->
-                                   <!-- <p class="card-subtitle card-subtitle-dash">You have 50+ new requests</p> -->
-                                  </div>
-                                  <div>
-                                    <a href="PoliceAsset.php"><button class="btn btn-primary btn-lg text-white mb-0 me-0" type="submit">Add new member</button></a>
-                                  </div>
-                                </div>
-                                <div class="table-responsive  mt-1 ">
-                                  <table class="table select-table ">
+                  <form action="#" method="POST">
+                    <table class="table select-table " id="mytable">
                                     
-                                    <thead>
-                                      <tr >
-                                        <!-- <th>
-                                          <div class="form-check form-check-flat mt-0">
-                                            <label class="form-check-label">
-                                              <input type="checkbox" class="form-check-input" aria-checked="false"><i class="input-helper"></i></label>
-                                          </div>
-                                        </th> -->
-                                      
-                                        <th><strong class =" text text-dark">ID</strong></th>
-                                        <th class="text-dark">RegisterDate</th>
-                                        <th class="text-dark">Name</th>
-                                        <th class="text-dark">Height</th>
-                                        <th class="text-dark">Color</th>
-                                        <th class="text-dark">Weight</th>
-                                        <th class="text-dark">Quantity</th>
-                                        <th class="text-dark">Catogery</th>
-                                        <th class="text-dark">Store_Number</th>
-                                        <th class="text-dark">Gaurdian_Name</th>
-                                        <th class="text-dark">Gaurdian_ID</th>
-                                        <th class="text-dark">Condition</th>
-                                        <th class="text-dark">Note</th>
-                                        <th class="text-dark">Deliver</th>
-                                        <th class="text-dark">ACTION</th>
+                      <thead>
+                        <tr >
+                          <th><strong class =" text text-dark">ID</strong></th>
+                          <th class="text-dark">RegisterDate</th>
+                          <th class="text-dark">Name</th>
+                          <th class="text-dark">Height</th>
+                          <th class="text-dark">Color</th>
+                          <th class="text-dark">Weight</th>
+                          <th class="text-dark">Quantity</th>
+                          <th class="text-dark">Catogery</th>
+                          <th class="text-dark">Store_Number</th>
+                          <th class="text-dark">Gaurdian_Name</th>
+                          <th class="text-dark">Gaurdian_ID</th>
+                          <th class="text-dark">Condition</th>
+                          <th class="text-dark">Note</th>
+                          <th class="text-dark">Deliver</th>
+                          <th class="text-dark">ACTION</th>
+                          <th class="text-dark">ACTION</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <?php 
+                            include '../config.php';
+                            $result = mysqli_query($con, "SELECT * FROM assetregistration_table " );
+                              while ($row = mysqli_fetch_assoc($result)){
+                              $itemID = $row['id'];
+                            ?>
+                            <td><?php echo $itemID; ?></td>
+                            <td><?php echo $row['item_Register_Date']; ?></td>
+                            <td><?php echo @$row['Item_Name']; ?></td>
+                            <td><?php echo @$row['item_Hight']; ?></td>
+                            <td><?php echo @$row['item_Color']; ?></td>
+                            <td><?php echo @$row['item_Weight']; ?></td>
+                            <td><?php echo @$row['item_Quantity']; ?></td>
+                            <td><?php echo @$row['item_Catogery']; ?></td>
+                            <td><?php echo  @$row['Store_Number']; ?></td>
+                            <td><?php echo @$row['Guarding_Name']; ?></td>
+                            <td><?php echo @$row['Guarding_Police_ID'];  ?></td>
+                            <td><?php echo @$row['item_Condition'];  ?></td>
+                            <td><?php echo @$row['item_Note'];  ?></td>
+                            <td><?php echo @$row['item_Delivery'];  ?></td>
+                            <td><a href="PoliceAssetEdit.php?id=<?php echo $itemID; ?>"  class="btn btn-success">EDIT</a></td>
+                            <td><a href="PoliceAssetDelete.php?id=<?php echo $itemID; ?>"  class="btn btn-danger">DELETE</a></td>
+                          </tr>
+                        <?php  } ?>
+                      </tbody>
+                      <tfoot>
 
-
-                                        
-                                      </tr>
-                                      
-                                    </thead>
-                                    <tbody>
-                                      <tr>
-                                        <?php 
-                                         include '../config.php';
-
-                                      
-
-                                          $result = mysqli_query($con, "SELECT * FROM assetregistration_table " );
-                                          
-
-                                           while ($row = mysqli_fetch_assoc($result)){
-                                            $itemID = $row['id'];
-                                          
-                                         
-                                          ?>
-                                          <td>
-                                        <p>
-                                          <?php 
-                                            echo $itemID;
-                                           
-                                          ?>
-                                        </p>
-                                        </td>
-                                        <td>
-                                        <p>
-                                          <?php 
-                                            echo $row['item_Register_Date'];
-                                          ?>
-                                        </p>
-                                        </td>
-                                        <td>
-                                        <p>
-                                          <?php 
-                                            echo @$row['Item_Name'];
-                                          ?>
-                                        </p>
-                                        </td>
-                                        <td>
-                                        <p>
-                                          <?php 
-                                            echo @$row['item_Hight'];
-                                          ?>
-                                        </p>
-                                        </td>
-                                        <td>
-                                        <p>
-                                          <?php 
-                                            echo @$row['item_Color'];
-                                          ?>
-                                        </p>
-                                        </td>
-                                        <td>
-                                        <p>
-                                          <?php 
-                                            echo @$row['item_Weight'];
-                                          ?>
-                                        </p>
-                                        </td>
-                                        <td>
-                                        <p>
-                                          <?php 
-                                            echo @$row['item_Quantity'];
-                                          ?>
-                                        </p>
-                                        </td>
-                                        <td>
-                                        <p>
-                                          <?php 
-                                            echo @$row['item_Catogery'];
-                                          ?>
-                                        </p>
-                                        </td>
-                                        <td>
-                                        <p>
-                                          <?php 
-                                            echo  @$row['Store_Number'];
-                                          ?>
-                                        </p>
-                                        </td>
-                                        <td>
-                                        <p>
-                                          <?php 
-                                            echo @$row['Guarding_Name'];
-                                          ?>
-                                        </p>
-                                        </td>
-                                        
-                                        <td>
-                                        <p>
-                                          <?php 
-                                            echo @$row['Guarding_Police_ID']; 
-                                          ?>
-                                        </p>
-                                        </td>
-                                        <td>
-                                        <p>
-                                          <?php 
-                                            echo @$row['item_Condition']; 
-                                          ?>
-                                        </p>
-                                        </td>
-                                        <td>
-                                        <p>
-                                          <?php 
-                                            echo @$row['item_Note']; 
-                                          ?>
-                                        </p>
-                                        </td>
-                                        <td>
-                                        <p>
-                                          <?php 
-                                            echo @$row['item_Delivery']; 
-                                          ?>
-                                        </p>
-                                        </td>
-                                        
-                                        <td>
-                                          <a href="PoliceAssetEdit.php?id=<?php echo $itemID; ?>"  class="btn btn-success">EDIT</a>
-                                        </td>
-                                        <td>
-                                          <a href="PoliceAssetDelete.php?id=<?php echo $itemID; ?>"  class="btn btn-danger">DELETE</a>
-                                          
-                                        </td>
-                                        <td>
-                                        
-                                      
-                                        
-                                        </td>
-
-                                       
-                                      </tr>
-                                      <?php  
-
-
-                                        }
-
-                                      
-                                        ?>
-                                    </tbody>
-                                  </table>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                        </form>
-              </div>
-             </div>
-            </div>
-          </div>
+                      </tfoot>
+                    </table>
+                  </form>
+                </div>
         <!-- content-wrapper ends -->
 
-        <!-- partial:partials/_footer.html -->
-        <footer class="footer">
-          <div class="d-sm-flex justify-content-center justify-content-sm-between">
-            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Premium <a href="https://www.bootstrapdash.com/" target="_blank">Bootstrap admin template</a> from BootstrapDash.</span>
-            <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Copyright © 2021. All rights reserved.</span>
-          </div>
-        </footer>
-        <!-- partial -->
+       <!-- Footer Start -->
+
+       <!-- Footer end -->
       </div>
       <!-- main-panel ends -->
     </div>
@@ -288,6 +142,20 @@
   <script src="../js/Chart.roundedBarCharts.js"></script>
  
   <!-- End custom js for this page-->
+
+     <!-- Data table plugins -->
+
+  <script src="../DataTables/jQuery-3.6.0/jquery-3.6.0.js"></script>
+  <script src="../DataTables/DataTables-1.13.4/js/jquery.dataTables.min.js"></script>
+  <script  src="../DataTables/DataTables-1.13.4/js/dataTables.bootstrap5.min.js"></script>
+
+  <script> 
+  $(document).ready(function () {
+      $('#mytable').DataTable({
+        scrollX: true,
+      });
+  });
+  </script>
 </body>
 
 </html>
