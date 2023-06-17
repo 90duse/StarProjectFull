bda9d94108f8e953760dbe31b9dd97c3f85b03b9
90duse <duseibnuduse1212@gmail.com>
2023-06-17 11:56:12 +0300

    added some change

--- template/config/script.js
+++ template/config/script.js
@@ -6,11 +6,11 @@ $(document).ready(function () {
 // table.buttons().container().appendTo('#example_wrapper.col-md:eq(0)')
 
 // });
-// function validateForm() {
-//     let x = document.forms["myForm"]["btncrime"].value;
-//     if (x == "") {
-//       alert("Crime Place must be filled out");
-//       return false;
-//     }
-//   };
+function validateForm() {
+    let x = document.forms["myForm"]["btncrime"].value;
+    if (x == "") {
+      alert("Crime Place must be filled out");
+      return false;
+    }
+  };
 
