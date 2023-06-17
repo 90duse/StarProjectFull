a70250ca12319c5b3718b5673043058f2c6ca793
90duse <duseibnuduse1212@gmail.com>
2023-06-17 11:55:35 +0300

    Commented some of the code

--- template/config/confirmDelete.js
+++ template/config/confirmDelete.js
@@ -48,40 +48,40 @@ $(document).ready(function (){
   
 
   
-$(document).ready(function (){
-  $('#btndelete').on('click', function(e){
-    e.preventDefault();
-    var ID = $(this).val();
-    alert(ID);
+// $(document).ready(function (){
+//   $('#btndelete').on('click', function(e){
+//     e.preventDefault();
+//     var ID = $(this).val();
+//     alert(ID);
 
-    swal({
-  title: "Are You Sure ",
-  text: "Once deleted, you will not be able to recover this file!",
-  icon: "warning",
-  buttons: true,
-  dangerMode: true,
-})
-.then((willDelete) => {
-  if (willDelete) {
-        $.ajax({
-          type: "POST",
-          url: "crimeDelete.php",
-          data:
-          {
-            'id': ID,
-            '#btndelete': true
-          },
-          success: function (response) 
-          {
-                if (response == 200) {
-                  swal ("Success!", "Your Data is Deleted", "success");
-                } else if (response == 500){
-                  swal ("Success!", "Your Data is Deleted", "success");
-                }
-           };
-    });
-  }
-    });
-   });
-}); 
+//     swal({
+//   title: "Are You Sure ",
+//   text: "Once deleted, you will not be able to recover this file!",
+//   icon: "warning",
+//   buttons: true,
+//   dangerMode: true,
+// })
+// .then((willDelete) => {
+//   if (willDelete) {
+//         $.ajax({
+//           type: "POST",
+//           url: "crimeDelete.php",
+//           data:
+//           {
+//             'id': ID,
+//             '#btndelete': true
+//           },
+//           success: function (response) 
+//           {
+//                 if (response == 200) {
+//                   swal ("Success!", "Your Data is Deleted", "success");
+//                 } else if (response == 500){
+//                   swal ("Success!", "Your Data is Deleted", "success");
+//                 }
+//            };
+//     });
+//   }
+//     });
+//    });
+// }); 
   
