d6ad86abf3d48702e01fe11d75b6277d3202f85a
90duse <duseibnuduse1212@gmail.com>
2023-06-25 15:17:58 +0300

    added some change of logout button

--- template/partials/navbar.php
+++ template/partials/navbar.php
@@ -156,13 +156,15 @@
               <div class="dropdown-header text-center">
                 <img class="img-md rounded-circle" src="../images/POLICEL.png" width = "100"alt="Profile image">
                 <p class="mb-1 mt-3 font-weight-semibold">officer</p>
-                <p class="fw-light text-muted mb-0">test@gmail.com</p>
+                <p class="fw-light text-muted mb-0"><?php echo $_SESSION['username']; ?></p>
               </div>
               <a class="dropdown-item"><i class="dropdown-item-icon mdi mdi-account-outline text-primary me-2"></i> My Profile <span class="badge badge-pill badge-danger">1</span></a>
               <a class="dropdown-item"><i class="dropdown-item-icon mdi mdi-message-text-outline text-primary me-2"></i> Messages</a>
               <a class="dropdown-item"><i class="dropdown-item-icon mdi mdi-calendar-check-outline text-primary me-2"></i> Activity</a>
               <a class="dropdown-item"><i class="dropdown-item-icon mdi mdi-help-circle-outline text-primary me-2"></i> FAQ</a>
-              <a  href="../login.php" class="dropdown-item"><i class="dropdown-item-icon mdi mdi-power text-primary me-2"></i>Sign Out</a>
+              <form action="../../logout.php" method="POST">
+                <button type="submit" name="btnsignout" class="dropdown-item" ><i class="dropdown-item-icon mdi mdi-power text-primary me-2"></i>Sign Out</button>
+              </form>
             </div>
           </li>
         </ul>
