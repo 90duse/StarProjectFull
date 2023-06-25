96b0244b0f6fe430c979cba5da791dd49f0125e2
90duse <duseibnuduse1212@gmail.com>
2023-06-25 15:03:44 +0300

    Added some changes and upates in Admin-Template Index table

--- Admin-Template/config/index.php
+++ Admin-Template/config/index.php
@@ -1,3 +1,9 @@
+<?php
+include "session.php";
+
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -24,32 +30,14 @@
   <link rel="shortcut icon" href="../images/favicon.png" />
 </head>
 <body>
-  <div class="container-scroller">
-    <div class="row p-0 m-0 proBanner" id="proBanner">
-      <div class="col-md-12 p-0 m-0">
-        <div class="card-body card-body-padding d-flex align-items-center justify-content-between">
-          <div class="ps-lg-1">
-            <div class="d-flex align-items-center justify-content-between">
-              <p class="mb-0 font-weight-medium me-3 buy-now-text">POLICE ARE  FRIEND OF THE POPULATION</p>
-              
-            </div>
-          </div>
-          <!-- <div class="d-flex align-items-center justify-content-between">
-            <a href="https://www.bootstrapdash.com/product/star-admin-pro/"><i class="mdi mdi-home me-3 text-white"></i></a>
-            <button id="bannerClose" class="btn border-0 p-0">
-              <i class="mdi mdi-close text-white me-0"></i>
-            </button>
-          </div> -->
-        </div>
-      </div>
-    </div>
+ 
     <!-- partial:partials/_navbar.php -->
     <?php  include "../partials/navbar.php";?>
     <!-- partial -->
   
     <div class="container-fluid page-body-wrapper">
       <!-- partial:partials/_settings-panel.html -->
-      <div class="theme-setting-wrapper">
+      <!-- <div class="theme-setting-wrapper">
         <div id="settings-trigger"><i class="ti-settings"></i></div>
         <div id="theme-settings" class="settings-panel">
           <i class="settings-close ti-close"></i>
@@ -66,7 +54,7 @@
             <div class="tiles default"></div>
           </div>
         </div>
-      </div>
+      </div> -->
       <div id="right-sidebar" class="settings-panel">
         <i class="settings-close ti-close"></i>
         <ul class="nav nav-tabs border-top" id="setting-panel" role="tablist">
