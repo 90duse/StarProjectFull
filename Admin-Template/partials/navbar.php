8f94808df3af4d891ebed4fe92a2d5fa87389435
90duse <duseibnuduse1212@gmail.com>
2023-06-26 11:35:36 +0300

    added some changes

--- Admin-Template/partials/navbar.php
+++ Admin-Template/partials/navbar.php
@@ -19,8 +19,8 @@
       <div class="navbar-menu-wrapper d-flex align-items-top"> 
         <ul class="navbar-nav">
           <li class="nav-item font-weight-semibold d-none d-lg-block ms-0">
-            <h1 class="welcome-text">Good Morning <span class="text-black fw-bold">OFFICER</span></h1>
-            <h3 class="welcome-sub-text"> </h3>
+            <h1 class="welcome-text">Welcome <span class="text-black fw-bold">Captan</span></h1>
+            <h3 class="welcome-sub-text"></h3>
           </li>
         </ul>
         <ul class="navbar-nav ms-auto">
@@ -158,7 +158,7 @@
                 <p class="mb-1 mt-3 font-weight-semibold">officer</p>
                 <p class="fw-light text-muted mb-0"><?php echo $_SESSION['username']; ?></p>
               </div>
-              <a class="dropdown-item"><i class="dropdown-item-icon mdi mdi-account-outline text-primary me-2"></i> My Profile <span class="badge badge-pill badge-danger">1</span></a>
+              <a href="Admin_profile.php" class="dropdown-item"><i class="dropdown-item-icon mdi mdi-account-outline text-primary me-2"></i> My Profile <span class="badge badge-pill badge-danger">1</span></a>
               <a class="dropdown-item"><i class="dropdown-item-icon mdi mdi-message-text-outline text-primary me-2"></i> Messages</a>
               <a class="dropdown-item"><i class="dropdown-item-icon mdi mdi-calendar-check-outline text-primary me-2"></i> Activity</a>
               <a class="dropdown-item"><i class="dropdown-item-icon mdi mdi-help-circle-outline text-primary me-2"></i> FAQ</a>
