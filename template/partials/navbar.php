279a33c39f9c930fae95af11ed6704ff092e86ca
90duse <duseibnuduse1212@gmail.com>
2023-06-27 23:16:55 +0300

    added some update

--- template/partials/navbar.php
+++ template/partials/navbar.php
@@ -158,7 +158,7 @@
                 <p class="mb-1 mt-3 font-weight-semibold">officer</p>
                 <p class="fw-light text-muted mb-0"><?php echo $_SESSION['username']; ?></p>
               </div>
-              <a class="dropdown-item"><i class="dropdown-item-icon mdi mdi-account-outline text-primary me-2"></i> My Profile <span class="badge badge-pill badge-danger">1</span></a>
+              <a href="user_Profile.php" class="dropdown-item"><i class="dropdown-item-icon mdi mdi-account-outline text-primary me-2"></i> My Profile <span class="badge badge-pill badge-danger">1</span></a>
               <a class="dropdown-item"><i class="dropdown-item-icon mdi mdi-message-text-outline text-primary me-2"></i> Messages</a>
               <a class="dropdown-item"><i class="dropdown-item-icon mdi mdi-calendar-check-outline text-primary me-2"></i> Activity</a>
               <a class="dropdown-item"><i class="dropdown-item-icon mdi mdi-help-circle-outline text-primary me-2"></i> FAQ</a>
