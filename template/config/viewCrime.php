ac8ba592a954edf42960d427151c5e843b4f7188
90duse <duseibnuduse1212@gmail.com>
2023-07-05 10:28:30 +0300

    Added some update

--- template/config/viewCrime.php
+++ template/config/viewCrime.php
@@ -34,14 +34,7 @@
 </head>
 <body class="">
 
-  <div class="container-scroller ">
-    <div class="row p-0 m-0 proBanner" id="proBanner">
-      <div class="col-md-12 p-0 m-0">
-        <div class="card-body card-body-padding d-flex align-items-center justify-content-between">
-          
-        </div>
-      </div>
-    </div>
+ 
           <!-- partial:partials/_navbar.html -->
          <?php  include "../partials/navbar.php";?>
           <!-- partial -->
