aa12f9a28e0015b840c2846975ec86ea033c4fc9
90duse <duseibnuduse1212@gmail.com>
2023-07-07 17:06:14 +0300

    added some updates

--- template/config/PoliceDelete.php
+++ template/config/PoliceDelete.php
@@ -3,10 +3,7 @@ include 'session.php';
 include '../config.php';
 include  '/functions.php';
 
-// $successMessage =  "<div class='alert alert-warning alert-dismissible fade show' role='alert'>
-// <strong>Holy guacamole!</strong> You should check in on some of those fields below.
-// <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
-// </div>";
+
 
     if(isset($_GET['ID'])){
      $id = $_GET['ID']; 
