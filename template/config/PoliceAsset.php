dcfb80ec3e71b4c91e0714ac86b21b6e3395427d
90duse <duseibnuduse1212@gmail.com>
2023-07-07 17:06:03 +0300

    added some updates

--- template/config/PoliceAsset.php
+++ template/config/PoliceAsset.php
@@ -26,25 +26,7 @@
 </head>
 <body class="">
 
-  <div class="container-scroller ">
-    <div class="row p-0 m-0 proBanner" id="proBanner">
-      <div class="col-md-12 p-0 m-0">
-        <div class="card-body card-body-padding d-flex align-items-center justify-content-between">
-          <!-- <div class="ps-lg-1">
-            <div class="d-flex align-items-center justify-content-between">
-              <p class="mb-0 font-weight-medium me-3 buy-now-text">Free 24/7 customer support, updates, and more with this template!</p>
-              <a href="https://www.bootstrapdash.com/product/star-admin-pro/?utm_source=organic&utm_medium=banner&utm_campaign=buynow_demo" target="_blank" class="btn me-2 buy-now-btn border-0">Get Pro</a>
-            </div>
-          </div> -->
-          <!-- <div class="d-flex align-items-center justify-content-between">
-            <a href="https://www.bootstrapdash.com/product/star-admin-pro/"><i class="mdi mdi-home me-3 text-white"></i></a>
-            <button id="bannerClose" class="btn border-0 p-0">
-              <i class="mdi mdi-close text-white me-0"></i>
-            </button>
-          </div> -->
-        </div>
-      </div>
-    </div>
+
     <!-- partial:partials/_navbar.html -->
    <?php  include "../partials/navbar.php";?>
     <!-- partial -->
@@ -66,23 +48,10 @@
                   <?php  
                     include '../config.php'; 
 
-
                     // Varaible declartion
                     $successMessage = 'Congrate! Your Data has being Saved Successfully';
-                    $register_date = '';
-                    $itemName = '';
-                    $itemHight = '';
-                    $itemColor = '';
-                    $itemWeight = '';
-                    $itemQuantity ='';
-                    $itemCatogery = '';
-                    $storeNumber ='';
-                    $guardingName ='';
-                    $guardingID ='';
-                    $itemCondition ='';
-                    $itemNote = '';
-                    $deliveryName = '';
-
+                    $register_date = $itemName =  $itemHight = $itemColor = $itemWeight = $itemQuantity = 
+                    $itemCatogery =  $storeNumber = $guardingName =  $guardingID =  $itemCondition = $itemNote = $deliveryName = '';
                    
                       if($_SERVER["REQUEST_METHOD"] == "POST")
                       {
@@ -102,7 +71,7 @@
                           if(isset($_POST["btnsubmit"]))
                           {
                           //echo "working";
-                          //$assetID = $_POST['assetid'];
+                          $asset_id = $_POST['assetid'];
                           @$register_date = $_POST['assetdate'];
                           @$itemName = $_POST['itemname'];
                           @$itemHight = $_POST['itemheight'];
@@ -117,8 +86,9 @@
                           @$itemNote = $_POST['itemnote'];
                           @$deliveryName = $_POST['deliveryname'];
                           }
-                      $sql = "INSERT INTO `assetregistration_table` (`id`, `item_Register_Date`, `Item_Name`, `item_Hight`, `item_Color`, `item_Weight`, `item_Quantity`, `item_Catogery`, `Store_Number`, `Guarding_Name`, `Guarding_Police_ID`, `item_Condition`, `item_Note`, `item_Delivery`)
-                      VALUES ('', '$register_date', '$itemName', '$itemHight', '$itemColor', '$itemWeight', '$itemQuantity', '$itemCatogery', '$storeNumber', '$guardingName', '$guardingID', '$itemCondition', '$itemNote', '$deliveryName')";
+                          // inserting query 
+                      $sql = "INSERT INTO `assetregistration_table` (`id`,`item_id`, `item_Register_Date`, `Item_Name`, `item_Hight`, `item_Color`, `item_Weight`, `item_Quantity`, `item_Catogery`, `Store_Number`, `Guarding_Name`, `Guarding_Police_ID`, `item_Condition`, `item_Note`, `item_Delivery`)
+                      VALUES ('','$asset_id', '$register_date', '$itemName', '$itemHight', '$itemColor', '$itemWeight', '$itemQuantity', '$itemCatogery', '$storeNumber', '$guardingName', '$guardingID', '$itemCondition', '$itemNote', '$deliveryName')";
                    
                           if (mysqli_query($con, $sql)) 
                           {
@@ -138,9 +108,13 @@
                   <form class="forms-sample" action="#" method ="POST" >
                   <div class="container">
                       <div class="row">
-                        <div class="col-lg-6">
+                        <div class="col-lg-6" hidden="true">
                         <label for="exampleInputUsername1"></label>
-                        <input type="number" class="form-control" id="#" name="assetid" placeholder="Item ID">
+                        <input type="number" class="form-control" id="#" name="#" placeholder="Item ID">
+                        </div>
+                        <div class="col-lg-6" >
+                        <label for="exampleInputUsername1"></label>
+                        <input type="text" class="form-control" id="#" name="assetid" placeholder="Item ID">
                         </div>
                         <div class="col-lg-6">
                               <div class="row mt-4">
