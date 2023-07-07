58a8dd6e6ebd782c52a6be1a0a24b54301880b8d
90duse <duseibnuduse1212@gmail.com>
2023-07-07 17:03:23 +0300

    added some updates

--- policeWebsite/policeweb/index.php
+++ policeWebsite/policeweb/index.php
@@ -218,48 +218,6 @@
 				                	<li class="nav-setting">
 				                		<a href="#"><span class="lnr lnr-cog"></span></a>
 				                	</li><!--/.search-->
-				                    <li class="dropdown">
-				                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" >
-				                            <span class="lnr lnr-cart"></span>
-											<span class="badge badge-bg-1">2</span>
-				                        </a>
-				                        <ul class="dropdown-menu cart-list s-cate">
-				                            <li class="single-cart-list">
-				                                <a href="#" class="photo"><img src="assets/images/collection/arrivals1.png" class="cart-thumb" alt="image" /></a>
-				                                <div class="cart-list-txt">
-				                                	<h6><a href="#">arm <br> chair</a></h6>
-				                                	<p>1 x - <span class="price">$180.00</span></p>
-				                                </div><!--/.cart-list-txt-->
-				                                <div class="cart-close">
-				                                	<span class="lnr lnr-cross"></span>
-				                                </div><!--/.cart-close-->
-				                            </li><!--/.single-cart-list -->
-				                            <li class="single-cart-list">
-				                                <a href="#" class="photo"><img src="assets/images/collection/arrivals2.png" class="cart-thumb" alt="image" /></a>
-				                                <div class="cart-list-txt">
-				                                	<h6><a href="#">single <br> armchair</a></h6>
-				                                	<p>1 x - <span class="price">$180.00</span></p>
-				                                </div><!--/.cart-list-txt-->
-				                                <div class="cart-close">
-				                                	<span class="lnr lnr-cross"></span>
-				                                </div><!--/.cart-close-->
-				                            </li><!--/.single-cart-list -->
-				                            <li class="single-cart-list">
-				                                <a href="#" class="photo"><img src="assets/images/collection/arrivals3.png" class="cart-thumb" alt="image" /></a>
-				                                <div class="cart-list-txt">
-				                                	<h6><a href="#">wooden arn <br> chair</a></h6>
-				                                	<p>1 x - <span class="price">$180.00</span></p>
-				                                </div><!--/.cart-list-txt-->
-				                                <div class="cart-close">
-				                                	<span class="lnr lnr-cross"></span>
-				                                </div><!--/.cart-close-->
-				                            </li><!--/.single-cart-list -->
-				                            <li class="total">
-				                                <span>Total: $0.00</span>
-				                                <button class="btn-cart pull-right" onclick="window.location.href='#'">view cart</button>
-				                            </li>
-				                        </ul>
-				                    </li><!--/.dropdown-->
 				                </ul>
 				            </div><!--/.attr-nav-->
 				            <!-- End Atribute Navigation -->
@@ -280,7 +238,7 @@
 				                    <li class=" scroll active"><a href="#home">home</a></li>
 				                    <li class="scroll"><a href="#feature">About The Police</a></li>
 				                    <li class="scroll"><a href="#blog">blog</a></li>
-				                    <li class="scroll"><a href="#newsletter">contact</a></li>
+				                    <li class=""><a href="reportfile.php">contact</a></li>
 				                </ul><!--/.nav -->
 				            </div><!-- /.navbar-collapse -->
 				        </div><!--/.container-->
@@ -364,45 +322,18 @@
 
 		
 
-		<!--sofa-collection start -->
-		<section id="sofa-collection">
-			<div class="owl-carousel owl-theme" id="collection-carousel">
-				<div class="sofa-collection collectionbg1">
-					<div class="container">
-						<div class="sofa-collection-txt">
-							<h2>unlimited sofa collection</h2>
-							<p>
-								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-							</p>
-							<div class="sofa-collection-price">
-								<h4>strting from <span>$ 199</span></h4>
-							</div>
-							<button class="btn-cart welcome-add-cart sofa-collection-btn" onclick="window.location.href='#'">
-								view more
-							</button>
-						</div>
-					</div>	
-				</div><!--/.sofa-collection-->
-				<div class="sofa-collection collectionbg2">
-					<div class="container">
-						<div class="sofa-collection-txt">
-							<h2>unlimited dainning table collection</h2>
-							<p>
-								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-							</p>
-							<div class="sofa-collection-price">
-								<h4>strting from <span>$ 299</span></h4>
-							</div>
-							<button class="btn-cart welcome-add-cart sofa-collection-btn" onclick="window.location.href='#'">
-								view more
-							</button>
-						</div>
+		<section>
+			<div class="container">
+				<div class="row">
+					<div class="col-6">
+                      <input type="text" class="form-control">
 					</div>
-				</div><!--/.sofa-collection-->
-			</div><!--/.collection-carousel-->
+					<div class="col-6">
 
-		</section><!--/.sofa-collection-->
-		<!--sofa-collection end -->
+					</div>
+				</div>
+			</div>
+		</section>
 
 		<!--feature start -->
 		<section id="feature" class="feature">
