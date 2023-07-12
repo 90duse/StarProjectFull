2874383add08ad70dcaf2ddbea9e0cd7e7203c6f
90duse <duseibnuduse1212@gmail.com>
2023-07-12 10:12:47 +0300

    Added some updates

--- template/config/optable.php
+++ template/config/optable.php
@@ -41,7 +41,9 @@
      <?php   include "../partials/sidebar.php";?>
      <div class="main-panel"> 
          <div class="content-wrapper">
-            <div class="container bg-primary p-3 text-center " >
+          <div class="card">
+          <div class="card-body">
+            <div class="container  p-3 text-center " >
                 <h4>OP</h4>
             
           <div class="row">
@@ -60,18 +62,16 @@
                       <a class="nav-link" id="contact-tab" data-bs-toggle="tab" href="#demographics" role="tab" aria-selected="false"></a>
                     </li>
                     <li class="nav-item">
-                      <a class="nav-link border-0" id="more-tab" data-bs-toggle="tab" href="#more" role="tab" aria-selected="false">More</a>
+                      <a class="nav-link border-0" id="" data-bs-toggle="tab" href="searchOpRecord.php" role="tab" aria-selected="false">Search</a>
                     </li>
                   </ul>
                   <div>
-                    <!-- <div class="btn-wrapper">
-                      <a href="#" class="btn btn-otline-dark align-items-center"><i class="icon-share"></i> Share</a>
-                      <a href="#" class="btn btn-otline-dark"><i class="icon-printer"></i> Print</a>
-                      <a href="#" class="btn btn-primary text-white me-0"><i class="icon-download"></i> Export</a>
-                    </div> -->
+                
                  </div>
             </div>
           </div>
+        </div>
+     </div>
       <!-- main-panel ends -->
     </div>
     <!-- page-body-wrapper ends -->
